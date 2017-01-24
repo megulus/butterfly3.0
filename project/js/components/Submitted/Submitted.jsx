1ecaa7a1a45e7136b4b324bb91af2e74987e4f88
Meg Dahlgren <meg.dahlgren@gmail.com>
2017-01-24 12:58:55 -0500

    add susy grid to Submitted component

--- project/js/components/Submitted/Submitted.jsx
+++ project/js/components/Submitted/Submitted.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
+import grid from '../../../scss/app.scss';
 import styles from './Submitted.css';
 
 class Submitted extends Component {
 
     render() {
         return (
-            <div className={styles.submitted}>
+            <div className={classNames(grid.centerColumn, styles.submitted)}>
                 <p className={styles.banner}>THANK YOU FOR THE EXTRA FEEDBACK</p>
                 <p>Have a nice day!</p>
                 <div></div>
